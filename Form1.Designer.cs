db5a9cfb06dbd8fcda06d30c28b9fdd02d59df9e
Questrox <frantsuzov.ilya@gmail.com>
2024-09-24 01:04:15 +0300

    Первая версия

---
+++ Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace Конструирование_ПО
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Admission_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Specialty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Form = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Duration = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Qualification = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FK_Field_of_study = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.FK_Department = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(0, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(730, 433);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(722, 407);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Учебные планы";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Admission_date,
+            this.Specialty,
+            this.Form,
+            this.Duration,
+            this.Qualification,
+            this.FK_Field_of_study,
+            this.FK_Department});
+            this.dataGridView1.Location = new System.Drawing.Point(1, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(720, 344);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(541, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сохранить изменения";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.button4);
+            this.tabPage4.Controls.Add(this.dataGridView4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(722, 407);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Направления подготовки";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(543, 356);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 41);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Сохранить изменения";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(726, 342);
+            this.dataGridView4.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.numericUpDown1);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(722, 407);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Отчет 1";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Показать дисциплины";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 133);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(717, 274);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(328, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Все дисциплины данного учебного плана:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(211, 13);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите учебный план:";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.dateTimePicker1);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(722, 407);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Отчет 2";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(6, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(559, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Факультеты, которые принимали учебные планы позже заданной даты:";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(1, 137);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.Size = new System.Drawing.Size(720, 269);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(10, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 28);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Показать факультеты";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(141, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(116, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Выберите дату:";
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Admission_date
+            // 
+            this.Admission_date.DataPropertyName = "Admission_date";
+            this.Admission_date.HeaderText = "Admission_date";
+            this.Admission_date.Name = "Admission_date";
+            // 
+            // Specialty
+            // 
+            this.Specialty.DataPropertyName = "Specialty";
+            this.Specialty.HeaderText = "Specialty";
+            this.Specialty.Name = "Specialty";
+            // 
+            // Form
+            // 
+            this.Form.DataPropertyName = "Form";
+            this.Form.HeaderText = "Form";
+            this.Form.Name = "Form";
+            // 
+            // Duration
+            // 
+            this.Duration.DataPropertyName = "Duration";
+            this.Duration.HeaderText = "Duration";
+            this.Duration.Name = "Duration";
+            // 
+            // Qualification
+            // 
+            this.Qualification.DataPropertyName = "Qualification";
+            this.Qualification.HeaderText = "Qualification";
+            this.Qualification.Name = "Qualification";
+            // 
+            // FK_Field_of_study
+            // 
+            this.FK_Field_of_study.DataPropertyName = "FK_Field_of_study";
+            this.FK_Field_of_study.HeaderText = "FK_Field_of_study";
+            this.FK_Field_of_study.Name = "FK_Field_of_study";
+            this.FK_Field_of_study.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.FK_Field_of_study.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // FK_Department
+            // 
+            this.FK_Department.DataPropertyName = "FK_Department";
+            this.FK_Department.HeaderText = "FK_Department";
+            this.FK_Department.Name = "FK_Department";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(728, 433);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Admission_date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Specialty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Form;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Duration;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Qualification;
+        private System.Windows.Forms.DataGridViewComboBoxColumn FK_Field_of_study;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FK_Department;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Конструирование_ПО
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["StudyPlanString"].ConnectionString;
+        SqlDataAdapter planAdapter, fieldOfStudyAdapter;
+
+        SqlCommandBuilder planBuilder, fieldOfStudyBuilder;
+        DataSet ds = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+
+            planAdapter = new SqlDataAdapter("select * from \"Plan\"", connectionString);
+            fieldOfStudyAdapter = new SqlDataAdapter("select * from \"Field_of_study\"", connectionString);
+            planBuilder = new SqlCommandBuilder(planAdapter);
+            fieldOfStudyBuilder = new SqlCommandBuilder(fieldOfStudyAdapter);
+            planAdapter.Fill(ds, "Plan");
+            fieldOfStudyAdapter.Fill(ds, "Field_of_study");
+            dataGridView1.DataSource = ds.Tables["Plan"];
+            dataGridView4.DataSource = ds.Tables["Field_of_study"];
+            FillPlanCombobox();
+        }
+
+        void FillPlanCombobox()
+        {
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns["FK_Field_of_study"]).DataSource =
+                ds.Tables["Field_of_study"];
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns["FK_Field_of_study"]).DisplayMember =
+                "Code";
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns["FK_Field_of_study"]).ValueMember =
+                "ID";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                planAdapter.Update(ds, "Plan");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при обновлении данных");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT Discipline.Name, PD.Volume FROM Plan_discipline AS PD\r" +
+                "JOIN Discipline ON Discipline.ID = PD.FK_Discipline\r" +
+                "WHERE FK_Plan = " + numericUpDown1.Value.ToString() + "\r" +
+                "ORDER BY Volume DESC";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                DataTable dataTable = new DataTable("report1");
+                dataTable.Columns.Add("Name");
+                dataTable.Columns.Add("Volume");
+                while (sqlDataReader.Read())
+                {
+                    DataRow row = dataTable.NewRow();
+                    row["Name"] = sqlDataReader["Name"];
+                    row["Volume"] = sqlDataReader["Volume"];
+                    dataTable.Rows.Add(row);
+                }
+                sqlDataReader.Close();
+                dataGridView2.DataSource = dataTable;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter("GetPlans", sqlConnection);
+                sqlAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlAdapter.SelectCommand.Parameters.Add(new SqlParameter("@date", SqlDbType.Date));
+                sqlAdapter.SelectCommand.Parameters["@date"].Value = dateTimePicker1.Value;
+
+
+                DataSet dataSet = new DataSet();
+                sqlAdapter.Fill(dataSet, "report2");
+
+                dataGridView3.DataSource = dataSet.Tables["report2"];
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                fieldOfStudyAdapter.Update(ds, "Field_of_study");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при обновлении данных");
+            }
+        }
+    }
+}
